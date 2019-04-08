b9776eaffd62ce5e41aecf797e822a29194f12ed
Andrew <N00153667@student.iadt.ie>
2019-04-08 23:12:41 +0100

    navbar burger

--- src/client/App.js
+++ src/client/App.js
@@ -7,29 +7,65 @@ import recipeLogo from "./images/recipeLogo.png";
 import './app.css';
 
 // 'main' Component. Sets up the React Router and respective routes
-const App = () => {
-  return(
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      burgerClass: ""
+    };
+
+    this.setBurgerActive = this.setBurgerActive.bind(this);
+  }
+
+  setBurgerActive() {
+    console.log("test");
+    if(this.state.open) {
+      this.setState ({
+        open:false,
+        burgerClass: ""
+      });
+    } else {
+      this.setState ({
+        open:true,
+        burgerClass: "is-active"
+      });
+    }
+  }
+
+  render() {
+    let burgerClass = this.state.burgerClass;
+    return (
     <HashRouter>
       <div>
-        <nav className="navbar" role="navigation">
+        <nav className="navbar" role="navigation" aria-label="main navigation">
           <div className="navbar-brand">
-          <Link to="/About">
-            <img src={recipeLogo} alt="" className="navbar-image"/>
-          </Link>
+            <Link to="/About">
+              <img src={recipeLogo} alt="" className="navbar-image"/>
+            </Link>
+
+            <a role="button" className={"navbar-burger " + "burger " + burgerClass} onClick={this.setBurgerActive} aria-label="menu" aria-expanded="false" data-target="navbarMenu">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+
           </div>
-          <div className="navbar-menu">
 
-            <Link to="/About/" className="navbar-item">
-              About
-            </Link>
+          <div id="navbarMenu" className="navbar-menu">
+            <div className="navbar-start">
+              <Link to="/About/" className="navbar-item">
+                About
+              </Link>
 
-            <Link to="/" className="navbar-item">
-              Search
-            </Link>
+              <Link to="/" className="navbar-item">
+                Search
+              </Link>
 
-            <Link to="/RecipesList" className="navbar-item">
-              Saved Recipes
-            </Link>
+              <Link to="/RecipesList" className="navbar-item">
+                Saved Recipes
+              </Link>
+            </div>
 
           </div>
         </nav>
@@ -39,6 +75,7 @@ const App = () => {
       </div>
     </HashRouter>
   );
-};
+  }
+}
 
 export default App;
